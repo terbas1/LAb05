f5fe8b41a8945905b6cad908ba8d37c2ee61577d
Edwin Daniel Baltazar Terbullino <edwin.baltazar@tecsup.edu.pe>
2020-04-27 21:13:12 -0500

    first commit

---
+++ Business/BCategoria.cs
@@ -0,0 +1,77 @@
+﻿using Data;
+using Entity;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business
+{
+    public class BCategoria
+    {
+        private DCategoria dCategoria = null;
+
+        public List<Categoria> Get(int idcategoria)
+        {
+
+            List<Categoria> categoriaList = null;
+
+            try
+            {
+                dCategoria = new DCategoria();
+                categoriaList = dCategoria.Get();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            return categoriaList;
+        }
+
+        public void Insert(Categoria categoria)
+        {
+
+            try
+            {
+                dCategoria = new DCategoria();
+                categoria.IdCategoria = dCategoria.Get().Max(x => x.IdCategoria) + 1;
+                dCategoria.Insert(categoria);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public void Update(Categoria categoria)
+        {
+
+            try
+            {
+                dCategoria = new DCategoria();
+                dCategoria.Update(categoria);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public void Delete(int idcategoria)
+        {
+            try
+            {
+                dCategoria = new DCategoria();
+                dCategoria.Delete(idcategoria);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+    }
+}
---
+++ Semana05/ManCategoria.xaml.cs
@@ -0,0 +1,77 @@
+﻿using Business;
+using Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Semana05
+{
+    /// <summary>
+    /// Lógica de interacción para ManCategoria.xaml
+    /// </summary>
+    public partial class ManCategoria : Window
+    {
+        public Categoria categoria = new Categoria();
+
+        public ManCategoria(Categoria categoria)
+        {
+            InitializeComponent();
+
+            this.categoria = categoria;
+
+            if (this.categoria != null)
+            {
+                txtID.Text = this.categoria.IdCategoria.ToString();
+                txtNombre.Text = this.categoria.NombreCategoria;
+                txtDescripcion.Text = this.categoria.Descripcion;
+            }
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            BCategoria bCategoria = null;
+            try
+            {
+                bCategoria = new BCategoria();
+                if (categoria != null)
+                {
+                    bCategoria.Update(new Categoria { IdCategoria = int.Parse(txtID.Text), NombreCategoria = txtNombre.Text, Descripcion = txtDescripcion.Text });
+                }
+                else
+                {
+                    bCategoria.Insert(new Categoria { NombreCategoria = txtNombre.Text, Descripcion = txtDescripcion.Text });
+                }
+                Close();
+            }
+            catch (Exception)
+            {
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            BCategoria bCategoria = new BCategoria();
+            if (categoria != null)
+            {
+                bCategoria.Delete(categoria.IdCategoria);
+            }
+            Close();
+        }
+    }
+}
